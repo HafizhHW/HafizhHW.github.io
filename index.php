02aedad2b7be70c62213d54bd00867f519476ada
HafizhHW <apidsanjaya@gmail.com>
2023-08-11 15:45:51 +0700

    Firts Master Upload

---
+++ index.php
@@ -0,0 +1,168 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Hafizh & Sekar Wedding</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+
+    <!-- Fonts Google -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Sacramento&family=Work+Sans:wght@100;400;600;700&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="assets/myStyle.css" />
+
+    <!-- simplyCountdown -->
+    <link rel="stylesheet" href="countdown/simplyCountdown.theme.default.css" />
+    <script src="countdown/simplyCountdown.min.js"></script>
+
+
+</head>
+
+<body>
+
+    <section id="hero" class="hero w-100 h-100 p-3 mx-auto text-center d-flex justify-content-center align-items-center text-white">
+        <main>
+            <!-- <h2>The Wedding of</h2> -->
+            <div class="cropper border border-3 border-light shadow mb-4 mx-auto">
+                <img src="./assets/images/spousehug.jpg" alt="bg">
+            </div>
+            <h1>Hafizh & Sekar</h1>
+            <h3>Rabu, 31 April 2024</h3>
+            <h4>Kepada Bapak/Ibu/Saudara/i,</h4>
+            <h4>
+                <div id="namatamu"></div>
+            </h4>
+            <p>Di Tempat</p>
+            <p>Mohon maaf bila ada kesalahan pada penulisan nama/gelar</p>
+            <!-- <p>Akan melangsungkan resepsi pernikaha dalam: </p> -->
+            <!-- <div class="simply-countdown"></div> -->
+            <a href="#undangan" class="btn btn-lg mt-4">Lihat Undangan</a>
+        </main>
+    </section>
+
+    <!-- NavBar -->
+
+    <nav class="navbar navbar-expand-md bg-transparent sticky-top mynavbar">
+        <div class="container">
+            <a class="navbar-brand" href="#">Seha</a>
+            <button class="navbar-toggler border-0" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+                <div class="offcanvas-header">
+                    <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Seha</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                </div>
+                <div class="offcanvas-body">
+                    <div class="navbar-nav ms-auto">
+                        <a class="nav-link" href="#home">Home</a>
+                        <a class="nav-link" href="#info">Couple</a>
+                        <a class="nav-link" href="#story">Event</a>
+                        <a class="nav-link" href="#gallery">Gallery</a>
+                        <a class="nav-link" href="#rsvp">RSVP</a>
+                        <a class="nav-link" href="#gift">Gift</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
+
+    <section id="home" class="home">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-8 text-center">
+                    <h2>The Wedding of</h2>
+                    <h1>Hafizh & Sekar</h1>
+                    <h3>Rabu, 31 April 2024</h3>
+                    <p>Maha Suci Allah yang telah menciptakan pasangan-pasangan semuanya,baik dari apa yang ditumbuhkan oleh bumi dan dari diri mereka maupun dari apa yang tidak mereka ketahui.
+                        <br><br>Yasin (36:36)
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="couple" id="couple">
+        <div class="container">
+
+            <div class="row justify-content-center mx-auto text-center">
+                <div class="col-lg-5 col-md-8 col-sm-10 mb-5">
+                    <img class="" src="assets/images/bismillah.png" alt="">
+                </div>
+            </div>
+
+            <div class="row justify-content-center">
+                <div class="col text-center">
+                    <h3>Asslamu'alaikum Warahmatullahi Wabarakaatuh</h3>
+                    <p>Dengan memohon rahmat dan ridho Allah SWT kami bermaksud menyelengarakan pernikahan puta dan putri kami:</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="row">
+                        <div class="col-8 text-end">
+                            <h3>Hafizh Wijaya</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, quae.</p>
+                            <p>Putra dai Alm. Bpk Loram <br> dan <br> Ibu Ipsum </p>
+                        </div>
+                        <div class="col-4">
+                            <img src="assets/images/men.jpg" alt="Hafizh" class="img-resposive rounded-circle">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="row">
+                        <div class="col-4">
+                            <img src="assets/images/woman.jpg" alt="Sekar" class="img-resposive rounded-circle">
+                        </div>
+                        <div class="col-8">
+                            <h3>Sekar Ayu</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, quae.</p>
+                            <p>Putra dai Bpk Ipsum <br> dan <br> Ibu Lorem </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+    <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="./assets/myJS.js"></script>
+
+    <script>
+        simplyCountdown('.simply-countdown', {
+            year: 2024, // required
+            month: 4, // required
+            day: 31, // required
+            hours: 8, // Default is 0 [0-23] integer
+
+            words: { //words displayed into the countdown
+                days: {
+                    singular: 'Hari',
+                    plural: 'Hari'
+                },
+                hours: {
+                    singular: 'Jam',
+                    plural: 'Jam'
+                },
+                minutes: {
+                    singular: 'Menit',
+                    plural: 'Menit'
+                },
+                seconds: {
+                    singular: 'Detik',
+                    plural: 'Detik'
+                }
+            },
+        });
+    </script>
+</body>
+
+</html>
